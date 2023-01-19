e5d057cbe525f4c8160f129aa6bfdc4ee642ada4
Valentina Danilova <valentinka-danilova@mail.ru>
2023-01-19 23:28:30 +0300

    rules ffor formulas and some tests

--- verifier/src/main/kotlin/itmo/verifier/formula/Formula.kt
+++ verifier/src/main/kotlin/itmo/verifier/formula/Formula.kt
@@ -9,7 +9,6 @@ import com.github.h0tk3y.betterParse.parser.Parser
 sealed class CTLFormula
 
 object TRUE: CTLFormula()
-object FALSE: CTLFormula()
 
 data class Element(val name: String): CTLFormula()
 
@@ -17,47 +16,12 @@ data class Not(
     val formula: CTLFormula
 ): CTLFormula()
 
-data class And(
-    val left: CTLFormula,
-    val right: CTLFormula
-): CTLFormula()
-
 data class Or(
     val left: CTLFormula,
     val right: CTLFormula
 ): CTLFormula()
 
-data class Impl(
-    val left: CTLFormula,
-    val right: CTLFormula
-): CTLFormula()
-
-data class Eq(
-    val left: CTLFormula,
-    val right: CTLFormula
-): CTLFormula()
-
-data class AX(
-    var formula: CTLFormula
-): CTLFormula()
-
 data class EX(
-    var formula: CTLFormula
-): CTLFormula()
-
-data class AF(
-    var formula: CTLFormula
-): CTLFormula()
-
-data class EF(
-    var formula: CTLFormula
-): CTLFormula()
-
-data class AG(
-    var formula: CTLFormula
-): CTLFormula()
-
-data class EG(
     var formula: CTLFormula
 ): CTLFormula()
 
@@ -104,12 +68,12 @@ object CTLGrammar: Grammar<CTLFormula>() {
     val bracedExpression by -lpar * parser(this::implChain) * -rpar
 
 
-    val axOp by -ax * parser(this::ctlExpr) map { f -> AX(f)}
+    val axOp by -ax * parser(this::ctlExpr) map { f -> Not(EX(Not(f)))}
     val exOp by -ex * parser(this::ctlExpr) map { f -> EX(f)}
-    val afOp by -af * parser(this::ctlExpr) map { f -> AF(f)}
-    val efOp by -ef * parser(this::ctlExpr) map { f -> EF(f)}
-    val agOp by -ag * parser(this::ctlExpr) map { f -> AG(f)}
-    val egOp by -eg * parser(this::ctlExpr) map { f -> EG(f)}
+    val afOp by -af * parser(this::ctlExpr) map { f -> AU(TRUE, f)}
+    val efOp by -ef * parser(this::ctlExpr) map { f -> EU(TRUE, f)}
+    val agOp by -ag * parser(this::ctlExpr) map { f -> Not(EU(TRUE, Not(f)))}
+    val egOp by -eg * parser(this::ctlExpr) map { f -> Not(AU(TRUE, Not(f)))}
     val auOp by -a * -lspar * parser(this::ctlExpr) * -u * parser(this::ctlExpr) * -rspar map {(a, b) -> AU(a, b)}
     val euOp by -e * -lspar * parser(this::ctlExpr) * -u * parser(this::ctlExpr) * -rspar map {(a, b) -> EU(a, b)}
 
@@ -117,7 +81,7 @@ object CTLGrammar: Grammar<CTLFormula>() {
 
     val ctlExpr: Parser<CTLFormula> by
             (tr asJust TRUE) or
-            (fal asJust FALSE) or
+            (fal asJust Not(TRUE)) or
             (name map { Element(it.text) }) or
             negation or
             bracedExpression or
@@ -130,17 +94,11 @@ object CTLGrammar: Grammar<CTLFormula>() {
             auOp or
             euOp
 
-    val eqChain by leftAssociative(ctlExpr, eq) { a, _, b -> Eq(a, b) }
-    val andChain by leftAssociative(eqChain, and) { a, _, b -> And(a, b) }
+    val eqChain by leftAssociative(ctlExpr, eq) { a, _, b -> Or(Not(Or(a, b)), Not(Or(Not(a), Not(b)))) }
+    val andChain by leftAssociative(eqChain, and) { a, _, b -> Not(Or(Not(a), Not(b))) }
     val orChain by leftAssociative(andChain, or) { a, _, b -> Or(a, b) }
-    val implChain by rightAssociative(orChain, impl) { a, _, b -> Impl(a, b) }
+    val implChain by rightAssociative(orChain, impl) { a, _, b -> Or(Not(a), b) }
 
 
     override val rootParser by implChain
-}
-
-fun main(args: Array<String>) {
-//    val expr = "EX [1 U phi]"
-    val expr = "!EF !phi"
-    println(CTLGrammar.parseToEnd(expr))
 }
--- verifier/src/test/kotlin/itmo/verifier/FormulaParserTest.kt
+++ verifier/src/test/kotlin/itmo/verifier/FormulaParserTest.kt
@@ -1,14 +1,81 @@
 package itmo.verifier
 
+import AU
+import CTLGrammar
+import EU
+import EX
+import Element
+import Not
+import Or
+import TRUE
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class FormulaParserTest {
 
     @Test
-    fun `simple formula test`() {
-        val formulaString = "!EF !phi"
+    fun `true test`() {
+        val formulaString = "1"
 
-        println(CTLGrammar.parseToEnd(formulaString))
+        assertEquals(CTLGrammar.parseToEnd(formulaString), TRUE)
     }
+
+    @Test
+    fun `and test`() {
+        val formulaString = "1 & 0"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), Not(Or(Not(TRUE), Not(Not(TRUE)))))
+    }
+
+    @Test
+    fun `impl test`() {
+        val formulaString = "1 -> abacaba"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), Or(Not(TRUE), Element("abacaba")))
+    }
+
+    @Test
+    fun `ex test`() {
+        val formulaString = "EX(future)"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), EX(Element("future")))
+    }
+
+    @Test
+    fun `ef test`() {
+        val formulaString = "EF(future)"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), EU(TRUE, Element("future")))
+    }
+
+    @Test
+    fun `eg test`() {
+        val formulaString = "EG(future)"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), Not(AU(TRUE, Not(Element("future")))))
+    }
+
+    @Test
+    fun `au test`() {
+        val formulaString = "A [ 1 U EX(future | 0)]"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), AU(TRUE, EX(Or(Element("future"), Not(TRUE)))))
+    }
+
+    @Test
+    fun `spaces test`() {
+        val formulaString = "\tA           [ \t   1 U EX(future \n | 0)\n]\n\n\n"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), AU(TRUE, EX(Or(Element("future"), Not(TRUE)))))
+    }
+
+
+    @Test
+    fun `brackets test`() {
+        val formulaString = "(1) -> (((EX future)))"
+
+        assertEquals(CTLGrammar.parseToEnd(formulaString), Or(Not(TRUE), EX(Element("future"))))
+    }
+
 }

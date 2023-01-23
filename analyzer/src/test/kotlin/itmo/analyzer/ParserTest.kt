8cb7342385d3dfb3900a3f08bf561de4691faa8f
Mervap <valeratep@mail.ru>
2023-01-23 18:47:09 +0100

    Fix test names

--- analyzer/src/test/kotlin/itmo/analyzer/ParserTest.kt
+++ analyzer/src/test/kotlin/itmo/analyzer/ParserTest.kt
@@ -3,6 +3,7 @@ package itmo.analyzer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
 import java.io.File
+import java.util.*
 import kotlin.test.assertEquals
 
 class ParserTest {
@@ -11,7 +12,7 @@ class ParserTest {
     fun javaUtilArrayDeque(testInfo: TestInfo) = doTest(testInfo)
 
     private fun doTest(testInfo: TestInfo) {
-        val testName = testInfo.testMethod.get().name
+        val testName = testInfo.testMethod.get().name.replaceFirstChar { it.titlecase(Locale.getDefault()) }
         val src = File("src/test/testData/parser/$testName.java")
         val after = File("src/test/testData/parser/$testName.ast")
         assertEquals(after.readText(), src.parseJavaCode().printAST())
